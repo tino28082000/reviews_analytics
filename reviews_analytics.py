58a059be93c4478b2a6dc377cc5027286e67b8e8
Weishow <tino28082000@gmail.com>
2020-10-21 15:42:45 +0800

    Updates

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,11 +1,21 @@
 data = []
+count = 0
+total = 0
+
 
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
-print("資料筆數", len(data))
-print("第一筆:", data[0])
-print('-----------------')
-print("第二筆:", data[1])
+		total = total + len(data[count])
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+
+average_length = total / count
+print("資料筆數共有：", len(data))
+print('每筆資料平均長度為：', average_length )
+
+
+
 
 
