4a8536ffb9c0340b4886421d64bdf0283d9bbfb8
Weishow <tino28082000@gmail.com>
2020-10-21 16:08:12 +0800

    Update

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,7 +1,8 @@
 data = []
 count = 0
 total = 0
-
+less = []
+good = []
 
 with open('reviews.txt', 'r') as f:
 	for line in f:
@@ -11,9 +12,22 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
+for d in data:
+	if len(d) < 100:
+		less.append(d) 
+
+for d in data:
+	if 'good' in d:
+		good.append(d)
+
 average_length = total / count
-print("資料筆數共有：", len(data))
-print('每筆資料平均長度為：', average_length )
+print("資料筆數共有:", len(data), '筆')
+print('每筆資料平均長度為:', average_length )
+print('留言少於100字的筆數有:', len(less), '筆')
+print('留言包含 Good 的筆數有:', len(good), '筆')
+
+
+
 
 
 
