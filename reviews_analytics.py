c4d780bf0de4f5399c0e8a73c0a2c4219042bdf5
Weishow <tino28082000@gmail.com>
2020-10-23 11:46:29 +0800

    Add Lisdt comprehension practice

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -20,11 +20,15 @@ for d in data:
 	if 'good' in d:
 		good.append(d)
 
+bad = [d for d in data if 'bad' in d #List comprehension
+
 average_length = total / count
 print("資料筆數共有:", len(data), '筆')
 print('每筆資料平均長度為:', average_length )
 print('留言少於100字的筆數有:', len(less), '筆')
 print('留言包含 Good 的筆數有:', len(good), '筆')
+print('總共有', len(bad), '筆資料裡面有提到 bad') 
+
 
 
 
